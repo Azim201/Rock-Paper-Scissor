d29e33d88072345cba2bbe96925df7ef3303ef81
Azim Ramlan <azimmuhd486@gmail.com>
2024-02-11 19:26:04 +0800

    rps-ui

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,6 +1,10 @@
 
+document.addEventListener('DOMContentLoaded', function(){
 
-function getComputerChoice(){
+let playerScore = 0;
+let computerScore = 0;
+
+    function getComputerChoice(){
 let choice = Math .floor(Math.random() *3);
 if (choice === 0){
     return "rock"
@@ -15,15 +19,16 @@ if (choice === 0){
 getComputerChoice();
 
 function play(playerSelection,computerSelection){
-   
-    
+  
+    playerSelection=playerSelection.toLowerCase();
+
     if(playerSelection === "rock" && computerSelection === "paper"){
         return "you lost! paper beat rock"
     }else if (playerSelection === "paper" && computerSelection === "rock"){
         return "you win! paper beat rock"
     }else if(playerSelection === "rock" && computerSelection === "scissor"){
         return "you win! rock beat scissor"
-    }else if(playerSelection==="scissor" && computerSelection === "rock"){
+    }else if(playerSelection=== "scissor" && computerSelection === "rock"){
         return "you lost! rock beat scissor"
     }else if(playerSelection === "paper" && computerSelection === "scissor"){
         return "you lost! scissor beat paper"
@@ -34,14 +39,60 @@ function play(playerSelection,computerSelection){
     }
 }
 
+function updScore(resultText){
+    if (resultText.includes("you win")){
+        playerScore++
+    }
+    else if(resultText.includes("you lost")){
+        computerScore++
+    }
+}
+
+function winner(){
+    if(playerScore == 5){
+        return "Congratulation, you are the winner!"
+    }
+    if(computerScore == 5){
+        return "sorry, you lost"
+    }
+}
+
+function updateDisplay(){
+    const score = document.querySelector('.score')
+    const scoreUpd = document.querySelector('.scoreUpd')
+
+    updScore(resultText);
+
+    const winnerMessage= winner();
+    if(winnerMessage){
+        score.textContent = winnerMessage
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+        score.textContent = ` player: ${playerScore} - computer: ${computerScore}`
+    }
+    scoreUpd.textContent = resultText
+
+      
+}
+
+
 
 function game(){
-   
-    for(let i =1 ; i<=5; i++){
-    playerSelection = prompt("Please choose either one (rock,paper or scissor)","rock").toLowerCase()
-    computerSelection = getComputerChoice()
-    console .log (play(playerSelection, computerSelection))
+
+    const button = document.querySelectorAll("#button button");
     
-    }
+    
+    button.forEach((button) => {
+        button.addEventListener('click' , () => {
+                const playerSelection = button.id
+                const computerSelection = getComputerChoice()
+                resultText = (play(playerSelection, computerSelection)) 
+                updateDisplay()                 
+    });
+    })
 } 
 game()
+})
+
+
